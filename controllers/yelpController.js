6895f40412354c196238aba7f51330e43517a84b
ski14hs <sean.k.ishikawa@gmail.com>
2019-01-26 10:33:58 -0800

    yelp controller

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const YelpKey = process.env.YELP_KEY;
 module.exports = {
 //Yelp functions
-  getYelpBusiness: function(term, location) {
-    return axios.get("https://api.yelp.com/v3/businesses/search?term=" + term + "&location=" + location, { headers: { Authorization: `Bearer ${YelpKey}` } });
+  getYelpBusiness: function(req, res) {
+    return axios.get("https://api.yelp.com/v3/businesses/search?term=" + req.body.term + "&location=" + location, { headers: { Authorization: `Bearer ${YelpKey}` } });
   }
 }
---
+++ routes/api/yelp.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const yelpController = require("../../controllers/yelpController");
+
+// Matches with "/api/travels"
+router.route("/")
+  .get(travelsController.findAll)
+  .post(travelsController.create);
+
+// Matches with "/api/travels/:id"
+router
+  .route("/:id")
+  .get(travelsController.findById)
+  .put(travelsController.update)
+  .delete(travelsController.remove);
+
+module.exports = router;
