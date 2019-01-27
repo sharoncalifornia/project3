e6a1bba145416409bb865920fd8c309b2d1081c5
unknown <ming29398@gmail.com>
2019-01-26 18:11:44 -0800

    fixed logo issue

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark">
-      <a className="navbar-brand" href="/"><image id="logo" alt="Logo" src="/images/logo.svg" width="65" height="65"> </image> <span>Destination Expert</span></a>
+      <a className="navbar-brand" href="/"><img id="logo" alt="Logo" src="/images/logo.svg" width="65" height="65"></img> <span>Destination Expert</span></a>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <button className="btn navbtn flex-grow-2 ml-3 mr-3" id="login" name="login">Login</button>
